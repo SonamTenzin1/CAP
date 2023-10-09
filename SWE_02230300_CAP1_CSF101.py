945b866745539e9b3c4e0f3dc0ad1eab30a814e2
SonamTenzin1 <02230300.cst@rub.edu.bt>
2023-10-09 15:35:25 +0600

    k

---
+++ SWE_02230300_CAP1_CSF101.py
@@ -0,0 +1,19 @@
+import requests
+from urllib.parse import quote
+import time
+
+while True:
+
+    def send_message(message):
+        token = '6372306748:AAGJMdxyunQ7YnEgXQvblwXH9fytoGOC2O4'
+        chat_id = '1677618396'  # Replace with the actual chat_id
+        message = quote(message)
+        url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+        print(requests.get(url).json())
+
+    send_message('remember to drink water')
+    time.sleep(10)
+
+
+
+
